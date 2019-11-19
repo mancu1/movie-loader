c600be708459fc9d1034be086609c45bf43d1dc1
Zakirov Mansur <mz@dsans.com>
2019-11-19 03:59:29 +0300

    create vue project
    project from vue-cli template

---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+export const RestApiUrl = 'https://api.themoviedb.org/3';
+export const ApiKey = '4e04289ef53fb7f2293cb749ec136f22';
+export const ImgApiUrl = 'https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg';
