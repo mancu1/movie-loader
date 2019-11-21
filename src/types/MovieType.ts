d28fd435c8d13507efad695d320c9a03a4955fb1
Zakirov Mansur <mz@dsans.com>
2019-11-21 03:13:36 +0300

    add types for api objects

---
+++ src/types/MovieType.ts
@@ -0,0 +1,17 @@
+export interface MovieType {
+  poster_path?: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: Number[];
+  id: Number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path?: string;
+  popularity: Number;
+  vote_count: Number;
+  video: Boolean;
+  vote_average: Number;
+
+}
