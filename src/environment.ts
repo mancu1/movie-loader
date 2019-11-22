4d433b18a80ddc8cbfb368b09ca4d4462ac8ac56
Zakirov Mansur <mz@dsans.com>
2019-11-22 04:10:32 +0300

    use Russian language

--- src/environment.ts
+++ src/environment.ts
@@ -5,5 +5,5 @@ export const ApiKey = '4e04289ef53fb7f2293cb749ec136f22';
 export const ImgApiUrl = 'https://image.tmdb.org/t/p/original';
 
 export function getRequestUrl(path: string, obj: any) {
-  return `${RestApiUrl + path}?${queryString.stringify({ ...obj, api_key: ApiKey, language: 'en-EN' })}`;
+  return `${RestApiUrl + path}?${queryString.stringify({ ...obj, api_key: ApiKey, language: 'ru-RU' })}`;
 }
